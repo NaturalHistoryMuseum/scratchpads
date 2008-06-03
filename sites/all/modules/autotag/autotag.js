1b7fcf8adb4eb8be6a84787058d05de20a366c34
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-06-03 08:44:07 +0000

    Warning message for server down time, and autotag nonsense
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@504 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/autotag/autotag.js
+++ sites/all/modules/autotag/autotag.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
   	hoverClass: 'autotag-dnd-drop-hover',
   	drop: function(ev, ui) {
   		ui.draggable.appendTo("#autotag-dnd-right>ul");
+  		autotag_update_form();
   	}
   });
   $ui('#autotag-dnd-left').droppable({
@@ -16,28 +17,39 @@ $(document).ready(function () {
   	hoverClass: 'autotag-dnd-drop-hover',
   	drop: function(ev, ui) {
   		ui.draggable.appendTo("#autotag-dnd-left>ul");
+  		autotag_update_form();
   	}
   });
-  $ui('#autotag-dnd-right>ul').droppable({
+  $ui('#autotag-dnd-right>*').droppable({
   	accept: ".autotag-dragable",
   	activeClass: 'autotag-dnd-drop-active',
   	hoverClass: 'autotag-dnd-drop-hover',
   	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-right");
+  		ui.draggable.appendTo("#autotag-dnd-right>ul");
+  		autotag_update_form();
   	}
   });
-  $ui('#autotag-dnd-left>ul').droppable({
+  $ui('#autotag-dnd-left>*').droppable({
   	accept: ".autotag-dragable",
   	activeClass: 'autotag-dnd-drop-active',
   	hoverClass: 'autotag-dnd-drop-hover',
   	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-left");
+  		ui.draggable.appendTo("#autotag-dnd-left>ul");
+  		autotag_update_form();
   	}
   });
   $ui('.autotag-dragable').droppable({
   	accept: ".autotag-dragable",
   	drop: function(ev, ui) {
   		ui.draggable.appendTo($(this).parent());
+  		autotag_update_form();
   	}
   });
 });
+function autotag_update_form(){
+  var hidden_tids = '';
+  $.each($ui('#autotag-dnd-right>ul>*'), function(){
+    hidden_tids += '|'+$ui(this).attr('tid');
+  });
+  $('#edit-hideterms').val(hidden_tids);
+}
