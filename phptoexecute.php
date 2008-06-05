f4d4f128b4c0ac1cad01123627adfad30d863e08
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-06-05 10:36:11 +0000

    Quick change
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@518 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- phptoexecute.php
+++ phptoexecute.php
@@ -338,6 +338,7 @@ variable_set('mollom_fallback', "MOLLOM_STATUS_ACCEPT");*/
 $vocabs = taxonomy_get_vocabularies();
 foreach($vocabs as $vocab){
   if($vocab->multiple == 1 && $vocab->tags == 0){
-    echo "   ".$vocab->name."\n";
+    if($vocab->name != 'Document store' && $vocab->name != 'Image Galleries')
+      echo "   ".$vocab->name."\n";
   }
 }
