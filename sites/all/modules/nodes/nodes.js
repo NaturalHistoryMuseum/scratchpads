9a87c75c1fcd119d07f54f403edcb99ecfba93dd
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-08-07 15:46:30 +0000

    Updated addnode module to HEAD version from Drupal.org (the previous "stable" version was fucked)
    Further development on the nodes module.  It actually works
    
    
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@780 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/nodes/nodes.js
+++ sites/all/modules/nodes/nodes.js
@@ -1,5 +1,25 @@
-function nodes(id, nid , field){
-  $('#'+id).hide();
-  $('#'+id).html('<h1>Stuff</h1>');  
-  $('#'+id).show();
+var lastform = false;
+function nodes(id, url){
+  // Hide other forms
+  if(lastform != id){
+    $(".nodes-form").remove();
+    // Get the HTML from server, and add it
+    var nodesGetForm = function (data) {
+      var returnHtml = Drupal.parseJson(data);
+      $('#'+id).html($('#'+id).html()+returnHtml['html']);
+    }
+    $.get(url, null, nodesGetForm);
+    lastform = id;
+  }
+}
+function nodescancel(){
+  $(".nodes-form").remove();
+  return false;
+}
+function nodessubmit(url,id,field){
+  var nodesSubmitForm = function (data) {
+    var returnHtml = Drupal.parseJson(data);
+    $('#'+field+'_'+returnHtml['nid']).html(returnHtml['value']);
+  }
+  $.post(url,{formvalue: $('#'+id).val(), formid: id, field: field, updated: $('#'+id+'-updated').val()},nodesSubmitForm);
 }
