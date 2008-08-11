dfa270ff16980174dd99f8118086e800d04c02f1
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-08-11 10:16:33 +0000

    Further nodes module shenanigans.  This module is actually close to being made available for beta testing.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@784 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/nodes/nodes.js
+++ sites/all/modules/nodes/nodes.js
@@ -7,9 +7,32 @@ function nodes(id, url){
     var nodesGetForm = function (data) {
       var returnHtml = Drupal.parseJson(data);
       $('#'+id).html($('#'+id).html()+returnHtml['html']);
+      
+      // Ensure fieldsets are collapsible - Christ, this is shite!
+      $('fieldset.collapsible > legend').each(function() {
+        var fieldset = $(this.parentNode);
+        // Expand if there are errors inside
+        if ($('input.error, textarea.error, select.error', fieldset).size() > 0) {
+          fieldset.removeClass('collapsed');
+        }
+        // Turn the legend into a clickable link and wrap the contents of the fieldset
+        // in a div for easier animation
+        var text = this.innerHTML;
+        $(this).empty().append($('<a href="#">'+ text +'</a>').click(function() {
+          var fieldset = $(this).parents('fieldset:first')[0];
+          // Don't animate multiple times
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        })).after($('<div class="fieldset-wrapper"></div>').append(fieldset.children(':not(legend)')));
+      });
+      // End of the shite
+      
     }
     $.get(url, null, nodesGetForm);
-    lastform = id;
+    lastform = id;    
   }
 }
 function nodescancel(){
