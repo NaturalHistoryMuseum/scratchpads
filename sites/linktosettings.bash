bcf8e2054a8661d41df5867588654d593d457c8a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-10-30 16:38:17 +0000

    Revert to r838
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@845 ccd0800f-5989-4abc-b5cb-522e550fd16a

---
+++ sites/linktosettings.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make doubly sure we're in the right directory.
+cd /var/www/html/sites
+
+# Get a list of all the directories and CD in to them
+for i in $( ls -l|grep ^d|sed -r "s|.*:[0-9][0-9]\ ||"|grep -v all$ ); do
+	echo $i
+	cd $i
+	rm settings.php
+	cp ../settings.php.default settings.php
+	chown apache:users settings.php
+	chmod 675 settings.php
+	cd ..
+done
