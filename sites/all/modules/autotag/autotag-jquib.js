ab00d778ea3b9db81e74c05f1fb1cc38b5e93cf0
Simon Rycroft <s.rycroft@nhm.ac.uk>
2008-11-17 16:35:05 +0000

    Whitespaces issues mainly (I think)
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@858 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/autotag/autotag-jquib.js
+++ sites/all/modules/autotag/autotag-jquib.js
@@ -2,47 +2,47 @@
 $(document).ready(function () {
   // Prevent duplicate attachment of the collapsible behavior.
   $ui('.autotag-dragable').draggable({
-		helper: 'clone', 
-		zIndex: 150,
-		appendTo: '#autotag-container',
-	});
+    helper: 'clone', 
+    zIndex: 150,
+    appendTo: '#autotag-container',
+  });
   $ui('#autotag-dnd-right').droppable({
-  	accept: ".autotag-dragable",
-  	activeClass: 'autotag-dnd-drop-active',
-  	hoverClass: 'autotag-dnd-drop-hover',
-  	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-right>ul");
-  	}
+    accept: ".autotag-dragable",
+    activeClass: 'autotag-dnd-drop-active',
+    hoverClass: 'autotag-dnd-drop-hover',
+    drop: function(ev, ui) {
+      ui.draggable.appendTo("#autotag-dnd-right>ul");
+    }
   });
   $ui('#autotag-dnd-left').droppable({
-  	accept: ".autotag-dragable",
-  	activeClass: 'autotag-dnd-drop-active',
-  	hoverClass: 'autotag-dnd-drop-hover',
-  	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-left>ul");
-  	}
+    accept: ".autotag-dragable",
+    activeClass: 'autotag-dnd-drop-active',
+    hoverClass: 'autotag-dnd-drop-hover',
+    drop: function(ev, ui) {
+      ui.draggable.appendTo("#autotag-dnd-left>ul");
+    }
   });
   $ui('#autotag-dnd-right>*').droppable({
-  	accept: ".autotag-dragable",
-  	activeClass: 'autotag-dnd-drop-active',
-  	hoverClass: 'autotag-dnd-drop-hover',
-  	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-right>ul");
-  	}
+    accept: ".autotag-dragable",
+    activeClass: 'autotag-dnd-drop-active',
+    hoverClass: 'autotag-dnd-drop-hover',
+    drop: function(ev, ui) {
+      ui.draggable.appendTo("#autotag-dnd-right>ul");
+    }
   });
   $ui('#autotag-dnd-left>*').droppable({
-  	accept: ".autotag-dragable",
-  	activeClass: 'autotag-dnd-drop-active',
-  	hoverClass: 'autotag-dnd-drop-hover',
-  	drop: function(ev, ui) {
-  		ui.draggable.appendTo("#autotag-dnd-left>ul");
-  	}
+    accept: ".autotag-dragable",
+    activeClass: 'autotag-dnd-drop-active',
+    hoverClass: 'autotag-dnd-drop-hover',
+    drop: function(ev, ui) {
+      ui.draggable.appendTo("#autotag-dnd-left>ul");
+    }
   });
   $ui('.autotag-dragable').droppable({
-  	accept: ".autotag-dragable",
-  	drop: function(ev, ui) {
-  		ui.draggable.appendTo($(this).parent());
-  	}
+    accept: ".autotag-dragable",
+    drop: function(ev, ui) {
+      ui.draggable.appendTo($(this).parent());
+    }
   });
 });
 function autotag_update_form(){
