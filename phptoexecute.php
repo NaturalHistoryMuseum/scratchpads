f39cfc372caabef6014059ff0adee9519051ded9
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-01-12 09:53:43 +0000

    Changes made
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@920 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- phptoexecute.php
+++ phptoexecute.php
@@ -469,8 +469,10 @@ node_access_rebuild();*/
 /*variable_set('biblio_openurlimage',"/sites/all/images/openurl.jpg");*/
 
 // Set the boost thingy to work
-/*variable_set('boost', 'CACHE_ENABLED');
-variable_set('boost_banner', '\n\n\n\n\n\n\n\n');
+variable_set('boost', 'CACHE_ENABLED');
+variable_set('boost_banner', '');
 variable_set('boost_file_path', 'cache');
 variable_set('preprocess_css', '1');
-variable_set('cache_lifetime', '86400');*/
+variable_set('cache_lifetime', '3600');
+
+echo "Finished";
