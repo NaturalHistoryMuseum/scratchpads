6dfb4f1943acb719bea3ddb592c677fd455cc51d
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-03-12 11:20:25 +0000

    Deleted
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1068 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/views_bonus/panels/views-bonus-panels-twocol-stack.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: views-bonus-panels-twocol-stack.tpl.php,v 1.1 2009/01/03 04:50:45 neclimdul Exp $
-/**
- * @file
- * Two column rendering template.
- */
-
-panels_load_include('display-render');
-print panels_print_layout($panel_name, $content);
