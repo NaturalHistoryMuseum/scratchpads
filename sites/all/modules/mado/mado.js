874bb15b77b049d598d327351004757e4ce047a5
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-03-27 15:43:29 +0000

    Almost complete, needs fully testing though.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1092 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/mado/mado.js
+++ sites/all/modules/mado/mado.js
@@ -1,8 +1,12 @@
 var mado_callback_url;
+var mado_finished_sorting;
 
 $(document).ready(function() {
   $('#mado-start-sort').click( function() {
+    $('#mado-start-sort').html(mado_finished_sorting);
     $('#divider').css('display','block');
+    $('#mado > *').css('background-color','white');
+    $('.resize').css('display','none');
     $('#mado .mado_content').each( function (){
       if($(this).height()>150){
         $(this).height($(this).height()/2);
@@ -25,4 +29,25 @@ $(document).ready(function() {
       }
     });
   });
+  $('.resize-x').click( function(){
+    $(this).parent().parent().parent().parent().parent().parent().toggleClass("mado_block_2");
+    mado_update_resizes(this);
+  });
+  $('.resize-y').click( function(){
+    $(this).parent().parent().parent().parent().children('.subboxcontent').children('.mado_content').toggleClass("mado_scroll");
+    mado_update_resizes(this);
+  });
 });
+function mado_update_resizes(element){
+  var scroll = 0;
+  if($(element).parent().parent().parent().parent().children('.subboxcontent').children('.mado_content').hasClass("mado_scroll")){
+    scroll = 1;
+  }
+  var newclass = $(element).parent().parent().parent().parent().parent().parent().attr("class");
+  var resizeid = $(element).parent().parent().parent().parent().parent().parent().attr("id");
+  var ajax_options = {
+    url:mado_callback_url,
+    data:{"resize":resizeid,"identifier":mado_ident,"class":newclass,"scroll":scroll}
+  };
+  $.ajax(ajax_options);  
+}
