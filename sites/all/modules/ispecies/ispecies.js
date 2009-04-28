272e9a11e7fbcd6c91041896c487d3ba3e6b361f
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-04-28 13:32:51 +0000

    Minor tidying, and added empty view text
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1185 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/ispecies/ispecies.js
+++ sites/all/modules/ispecies/ispecies.js
@@ -1,9 +1,7 @@
-var page_tid;
-
 function ispecies_callback(callback, viewname){
   var ajax_options = {
     cache:false,
-    url:callback+"/"+viewname+"/"+page_tid,
+    url:callback+"/"+viewname+"/"+Drupal.settings.ispecies.page_tid,
     success:function(data){
       ispecies_success(data, viewname);
     },
@@ -18,11 +16,16 @@ function ispecies_callback(callback, viewname){
 function ispecies_success(data, viewname){
   var resultObj = eval('(' + data + ')');
   var output = '';
+  var output_added = false;
   $.each(resultObj, function() {
     if(this['body']){
+      output_added = true;
       output += this['body'];
     }
   });
+  if(!output_added){
+    output = Drupal.settings.ispecies.empty_view;
+  }
   // Replace the output already set
   $('#'+viewname).html(output);
   // Get the citations
