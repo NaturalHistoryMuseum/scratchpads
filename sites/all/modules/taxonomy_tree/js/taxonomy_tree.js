c2667c3ca1f2885d1dfd1f14b5251dccf222ad84
Ben Scott <b.scott@nhm.ac.uk>
2009-05-20 15:35:09 +0000

    taxonomy tree module
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1278 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/taxonomy_tree/js/taxonomy_tree.js
+++ sites/all/modules/taxonomy_tree/js/taxonomy_tree.js
@@ -1,39 +1,53 @@
 
-function initTreeview(id, vid, selected) {
+function treeview(){
   
-  var url = Drupal.settings.taxonomyTreeCallbackPath+'/'+vid+selected;
-
-  $("#"+id).treeview({ 
-     url: url
-  });
+  var self;
+  
+  return {//
     
-  $("#"+id).ajaxSuccess(function(evt, request, settings){
-   
-     $("#"+id+" input").unbind('mousedown');
-     $("#"+id+" input").bind('mousedown', function(){
-          
-       $(this).attr('checked', ($(this).attr('checked') ? 0 : 1)); 
+    init: function(id, vid, selected) {
+  
+      self = this;
 
-     });
-     
-     $("#"+id+" input").click(function(){
+      $("#"+id).treeview({ 
+         url: Drupal.settings.taxonomyTreeCallbackPath+'/'+vid+'?'+selected,
+         persist: "cookie"
+      });
+      
+      $("#"+id).ajaxSuccess(function(){
+        self.updateEvents(id)
+      });
+      
+    },
+    
+    updateEvents: function(id){
+      
+       // Prevent cleciking the checkbox from expanding the tree
+      $("#"+id+" input").unbind('mousedown');
+      $("#"+id+" input").bind('mousedown', function(){
 
-       return false;
+        $(this).attr('checked', ($(this).attr('checked') ? 0 : 1)); 
 
-     });
-     
-     $("#"+id+" input").click(function(){
+      });
 
-       var id = $(this).parents('li').attr('id');
-       var split_id = [];
-       split_id = id.split('-');
-       $(this).parents('li').attr('id',split_id[0]+'-'+($(this).attr('checked') ? 0 : 1));
+      $("#"+id+" input").click(function(){
 
-     });
-     
-     
-   });
+        // Change the ID so child items are checked same as parent
+        var id = $(this).parents('li').attr('id');
+        var split_id = [];
+        split_id = id.split('-');
+        $(this).parents('li').attr('id',split_id[0]+'-'+($(this).attr('checked') ? 0 : 1));
+        return false;
 
+      });
+      
+      
+    }
+    
+  }
+  
 }
+
+
 
 
