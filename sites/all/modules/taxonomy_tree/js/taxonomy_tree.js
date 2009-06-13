c99343ca7d7ccd0156840e1cb31152cb4083a3d1
Ben Scott <b.scott@nhm.ac.uk>
2009-06-13 15:57:16 +0000

    Taxonomy tree bug fix
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1490 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/taxonomy_tree/js/taxonomy_tree.js
+++ sites/all/modules/taxonomy_tree/js/taxonomy_tree.js
@@ -1,84 +1,89 @@
-function treeview(){
+Drupal.behaviors.taxonomyTree = function(context) {
+
+  $(context).find('.taxonomy-tree-field ul').each(function(){
   
-  var self;
-  var elements = {};
+    // getTaxonomyTree();
+    var vid = $(this).attr('id');
   
-  return {//
+    var url = Drupal.settings.taxonomyTreeCallbackPath+'/'+vid;
     
-    init: function(id, vid, nid, selected, noTree) {
-
-      self = this;
-      
-      elements[id] = {
-        vid: vid,
-        nid: nid,
-        selected: selected       
-      }
-      
-      if(!noTree){
+    var nid = $(context).find("input[name='taxonomy_tree_nid["+vid+"]']").val();
+    var selected = $(context).find("input[name='taxonomy_tree_selected["+vid+"]']").val();
         
-        self.displayTree(id);
-        
-      }
+    if(typeof nid != 'undefined'){
       
-    },
-    
-    displayTree: function(id){
-
-      var element = elements[id];
-
-      $("#"+id).treeview({ 
-         url: Drupal.settings.taxonomyTreeCallbackPath+'/'+element['vid']+'?nid='+element['nid']+'&'+element['selected'],
-         persist: "cookie"
-      });
-      
-      $("#"+id).ajaxSuccess(function(){
-        self.updateEvents(id)
-      });
-      
-    },
-    
-    markAsSelected: function($obj, checked){
-      
-      var $li = $obj.parents('li');
-      var id = $li.attr('id');
-      
-      var split_id = [];
-      split_id = id.split('-');
-      
-      $li.attr('id',split_id[0]+'-'+checked);
-      
-    },
-    
-    updateEvents: function(element_id){
-      
-       // Prevent clicking the checkbox from expanding the tree
-      $("#"+element_id+" input").unbind('mousedown');
-      $("#"+element_id+" input").bind('mousedown', function(){
-
-        $(this).attr('checked', ($(this).attr('checked') ? 0 : 1)); 
-
-      });
-      
-      $("#"+element_id+" input:checked").each(function(){
-        
-        self.markAsSelected($(this), 1);
-        
-      });
-
-      $("#"+element_id+" input").click(function(){
-
-        self.markAsSelected($(this), ($(this).attr('checked') ? 0 : 1));
-        return false;
-
-      });
-      
+      url += '/'+nid;
       
     }
     
-  }
+    if(selected){
+    
+      url += '?t='+selected;
+    
+    }
+    
+    $(this).treeview({ 
+       url: url,
+       persist: "location",
+    });
+    
+    // updateEvents($(this));
+    
+    $(this).ajaxSuccess(function(){
+      updateEvents($(this))
+    });
   
+  });
+  
+
+
 }
+
+updateEvents = function($context){
+  
+   // Prevent clicking the checkbox from expanding the tree
+   
+  $context.find('input').each(function(){
+    
+    $(this).unbind('mousedown');
+    $(this).bind('mousedown', function(){
+    
+      $(this).attr('checked', ($(this).attr('checked') ? 0 : 1)); 
+    
+    });
+    
+    if($(this).is(':checked')){
+      
+      markAsSelected($(this), 1);
+      
+    }
+    
+    $(this).click(function(){
+    
+      self.markAsSelected($(this), ($(this).attr('checked') ? 0 : 1));
+      return false;
+    
+    });
+    
+  })
+
+}
+
+markAsSelected = function($obj, checked){
+  
+  var $li = $obj.parents('li');
+  var id = $li.attr('id');
+  
+  var split_id = [];
+  split_id = id.split('-');
+  
+  $li.attr('id',split_id[0]+'-'+checked);
+  
+};
+
+
+
+
 
 
 
