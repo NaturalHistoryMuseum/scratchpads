ef5aa2432c3c8cad5633f14d396d278c032a0a8e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-06-25 18:45:28 +0000

    Loads of mado tweaks.  Only thing that needs fixing is sorting after having changed the height/width of an element.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1595 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/mado/mado.js
+++ sites/all/modules/mado/mado.js
@@ -10,6 +10,11 @@ $(document).ready(function() {
     height: 'auto',
     title: 'Apply to "This page" or "All"'
   });
+  $('.mado_close').click(function(){
+    var this_id = $(this).parent().attr('id');
+    $('#'+this_id).insertAfter("#divider");
+    mado_after_sort();
+  });
   $('#mado-start-sort').click( function() {
     if(!currently_sorting){
       $('#mado-start-sort').html(Drupal.settings.mado.finished_sorting);
@@ -24,20 +29,27 @@ $(document).ready(function() {
         }
       });
       $('#mado').sortable({
+        start: function(event, ui){
+          $('.mado_close').css('display','none');
+        },
         stop: function(event, ui){
-          var items = Array();
-          $('.mado_block').each(function(){
-            if($(this).attr('id')!=''){
-              items[items.length] = $(this).attr('id');
-            }
-          });
-          var ajax_options = {
-            url:Drupal.settings.mado.callback_url,
-            data:{"blocks[]":items,"identifier":Drupal.settings.mado.ident}
-          };
-          $.ajax(ajax_options); 
+          mado_after_sort();
         }
       });
+      $('#mado > *').bind("mouseenter", function(){
+          // Mouse over a sort block
+          var position = $(this).position();
+          var width = $(this).width();
+          $(this).children('.mado_close').css('display','block');
+          $(this).children('.mado_close').css('top',position.top);
+          $(this).children('.mado_close').css('left',position.left+width-30);
+        }
+      );
+      $('#mado > *').bind("mouseleave", function(){
+          // Mouse out of a sort block
+          $(this).children('.mado_close').css('display','none');
+        }
+      );
     }
     currently_sorting = true;
   });
@@ -50,6 +62,19 @@ $(document).ready(function() {
     mado_update_resizes(this);
   });
 });
+function mado_after_sort(){
+  var items = Array();
+  $('.mado_block').each(function(){
+    if($(this).attr('id')!=''){
+      items[items.length] = $(this).attr('id');
+    }
+  });
+  var ajax_options = {
+    url:Drupal.settings.mado.callback_url,
+    data:{"blocks[]":items,"identifier":Drupal.settings.mado.ident}
+  };
+  $.ajax(ajax_options); 
+}
 function mado_update_resizes(element){
   var scroll = 0;
   if($(element).parent().parent().parent().parent().children('.subboxcontent').children('.mado_content').hasClass("mado_scroll")){
