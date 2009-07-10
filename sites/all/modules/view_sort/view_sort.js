447d430ff6f84a413f77342f0636939d58c5ad05
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-07-10 10:12:31 +0000

    Still working. Periodic commit
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1661 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/view_sort/view_sort.js
+++ sites/all/modules/view_sort/view_sort.js
@@ -36,6 +36,59 @@ function view_sort_add_draggable(viewname){
       $.ajax(ajax_options);
     }
   });
+  view_sort_add_mouseover();
+  // Change the number of items to display
+  $('#view-sort-select').change(function(){
+    // Change the number to show
+    var dropdown_number = $('#view-sort-select').val();
+    var ajax_options = {
+      type:"POST",
+      url:Drupal.settings.view_sort.callbacks.number,
+      data:{view:viewname, number:dropdown_number}
+    };
+    $.ajax(ajax_options);
+    // Count the number of blocks in the top
+    var top_div_sortables_count=0;
+    $('.view-sort-top > .sort-div').each(function(){
+      top_div_sortables_count++;
+    });
+    if(top_div_sortables_count == dropdown_number){return;}
+    else if(top_div_sortables_count > dropdown_number){
+      // Too many on top, move some down.
+      var number_to_move = top_div_sortables_count - dropdown_number;
+      var number_of_div = 1;
+      $('.view-sort-top > .sort-div').each(function(){
+        if(number_of_div > dropdown_number){
+          var something = $(this).clone();
+          something.prependTo(".view-sort-bottom");
+          $(this).remove();
+        }
+        number_of_div ++;
+      });
+    } else {
+      var number_to_move = dropdown_number - top_div_sortables_count;
+      var number_of_div = 1;
+      $('.view-sort-bottom > .sort-div').each(function(){
+        if(number_of_div <= number_to_move){
+          var something = $(this).clone();
+          something.appendTo(".view-sort-top"); 
+          $(this).remove();
+          view_sort_add_mouseover(); 
+        }
+        number_of_div ++;
+      });
+    }
+  });
+  $('#view-sort-error').dialog({
+    autoOpen: false,
+    bgiframe: true,
+    draggable: true,
+    modal: true,
+    height: 150
+  });
+}
+
+function view_sort_add_mouseover(){
   // Show the Lock/Hide images
   $('.view-sort-drag > div > *').mouseover(function(){
     $(this).children().each(function(){
@@ -51,22 +104,5 @@ function view_sort_add_draggable(viewname){
         $(this).hide();
       }
     })
-  });
-  // Change the number of items to display
-  $('#view-sort-select').change(function(){
-    // Change the number to show
-    var ajax_options = {
-      type:"POST",
-      url:Drupal.settings.view_sort.callbacks.number,
-      data:{view:viewname, number:$('#view-sort-select').val()}
-    };
-    $.ajax(ajax_options);
-  });
-  $('#view-sort-error').dialog({
-    autoOpen: false,
-    bgiframe: true,
-    draggable: true,
-    modal: true,
-    height: 150
   });
 }
