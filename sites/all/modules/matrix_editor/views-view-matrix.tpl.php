d72f197dd19f8d46ffa2ab1f55589c066ab701ac
Simon Rycroft <s.rycroft@nhm.ac.uk>
2009-10-08 12:37:41 +0000

    Format the poorly formatted code.
    THERE ARE NO CHANGES TO THE CODE, JUST FORMATTING.
    
    
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@1921 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/matrix_editor/views-view-matrix.tpl.php
+++ sites/all/modules/matrix_editor/views-view-matrix.tpl.php
@@ -1,9 +1,27 @@
-<!-- <div class="<?php print $class; ?>-wrapper">
-  <div id="<?php print $id; ?>" class="<?php print $class; ?>-wrapper" style="<?php print $style; ?>"></div>
-  <?php if (isset($controls)): ?>
-    <div id="<?php print $id; ?>-controls" class="<?php print $class; ?>-controls"></div>
-  <?php endif; ?>
+<!-- <div class="<?php
+print $class;
+?>-wrapper">
+  <div id="<?php
+  print $id;
+  ?>" class="<?php
+  print $class;
+  ?>-wrapper" style="<?php
+  print $style;
+  ?>"></div>
+  <?php
+  if(isset($controls)):
+    ?>
+    <div id="<?php
+    print $id;
+    ?>-controls" class="<?php
+    print $class;
+    ?>-controls"></div>
+  
+  <?php endif;
+  ?>
 </div> -->
 
 
-<?php print $matrix; ?>
+<?php
+print $matrix;
+?>
