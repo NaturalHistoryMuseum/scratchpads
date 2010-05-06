9f4bc800d45342099d77ab8edc9691e575b714d2
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-05-06 09:36:48 +0000

    Hides fields as required.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/branches/drupal6@2545 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/itis_term/itis_term.js
+++ sites/all/modules/itis_term/itis_term.js
@@ -10,6 +10,10 @@ Drupal.itis_term.init = function(context) {
   $('.itis_term_unit input', context).bind('focusout', function(){
     Drupal.itis_term.showorhide_unit_fields(context);
   });
+  $('#itis_term_usage select', context).change(function(){
+    Drupal.itis_term.showorhide_usage_fields(context);
+  });
+  Drupal.itis_term.showorhide_usage_fields(context);
   Drupal.itis_term.showorhide_unit_fields(context);
   $('#itis_term_name').hide();
 }
@@ -29,7 +33,6 @@ Drupal.itis_term.showorhide_unit_fields = function(context){
   } else {
     $('#itis_term_unit_name3', context).fadeIn(500);
     $('#itis_term_unit_ind2', context).fadeIn(500);
-    $('#itis_term_name input').val($('#itis_term_unit_name1 input', context).val()+" "+$('#itis_term_unit_name2 input', context).val());
   }
   if($('#itis_term_unit_name3 input', context).val() == ''){
     $('#itis_term_unit_name4', context).hide(500);
@@ -37,13 +40,21 @@ Drupal.itis_term.showorhide_unit_fields = function(context){
   } else {
     $('#itis_term_unit_name4', context).fadeIn(500);
     $('#itis_term_unit_ind3', context).fadeIn(500);
-    $('#itis_term_name input').val($('#itis_term_unit_name1 input', context).val()+" "+$('#itis_term_unit_name2 input', context).val()+" "+$('#itis_term_unit_name3 input', context).val());
   }
   if($('#itis_term_unit_name4 input', context).val() == ''){
     $('#itis_term_unit_ind4', context).hide(500);
   } else {
     $('#itis_term_unit_ind4', context).fadeIn(500);
-    $('#itis_term_name input').val($('#itis_term_unit_name1 input', context).val()+" "+$('#itis_term_unit_name2 input', context).val()+" "+$('#itis_term_unit_name3 input', context).val()+" "+$('#itis_term_unit_name4 input', context).val());
+  }
+}
+
+Drupal.itis_term.showorhide_usage_fields = function(context){
+  if($('#itis_term_usage select').val()==='invalid' || $('#itis_term_usage select').val()==='not accepted'){
+    $('#itis_term_accepted_name').fadeIn(500);
+    $('#itis_term_unacceptability_reason').fadeIn(500);
+  } else {
+    $('#itis_term_accepted_name').fadeOut(500);
+    $('#itis_term_unacceptability_reason').fadeOut(500);
   }
 }
 
