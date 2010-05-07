14a3e1b6c8d708735c05a09c9deb3b0a335f4cc2
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-05-07 10:34:58 +0000

    Added empty trunk.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@2567 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- sites/all/modules/countriesmap/countriesmap.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
- $('.countriesmapcheckbox').change(function (){
-  var check_status = $(this).is(':checked');
-  $('.' + $(this).attr('id')).each(function(){
-   $(this).attr('checked', check_status);
-  });
- });
-});
--- sites/all/modules/femail/femail.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('.femail-signature > p').bind('click', function(e){
-    $(this).parent().children('div').toggle('slow');
-  });
-});
