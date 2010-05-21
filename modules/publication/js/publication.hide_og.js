b8725f09e899620080983b9eb5a9b444fd676e8e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-05-21 13:20:45 +0000

    Added for colapsing publication added nodes.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@2780 ccd0800f-5989-4abc-b5cb-522e550fd16a

---
+++ modules/publication/js/publication.hide_og.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('#publication-display-og-ron').click(function(){
+    $('#publication-display-og-ron').hide();
+    $('.view-og-ghp-ron').slideDown(500);
+    return false;
+  });
+});
