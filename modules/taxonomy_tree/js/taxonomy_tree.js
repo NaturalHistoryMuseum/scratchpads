fd9b045dbc5db2714103b02b74efdf0d176fc4de
Ben Scott <b.scott@nhm.ac.uk>
2010-05-28 13:24:43 +0000

    Reworked taxonomy tree
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@2853 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/taxonomy_tree/js/taxonomy_tree.js
+++ modules/taxonomy_tree/js/taxonomy_tree.js
@@ -1,84 +1,43 @@
 Drupal.behaviors.taxonomyTree = function(context) {
 
-  $(context).find('.taxonomy-tree-field ul').each(function(){
-  
-    var vid = $(this).attr('id');
-  
-    var url = Drupal.settings.taxonomyTreeCallbackPath+'/'+vid;
-    
-    var nid = $(context).find("input[name='taxonomy_tree_nid["+vid+"]']").val();
-    var selected = $(context).find("input[name='taxonomy_tree_selected["+vid+"]']").val();
-        
-    if(typeof nid != 'undefined'){
-      
-      url += '/'+nid;
-      
-    }
-    
-    if(selected){
-    
-      url += '?t='+selected;
-    
-    }
-    
-    $(this).treeview({ 
-       url: url,
-       persist: "location",
-    });
-    
-    updateEvents($(this));
-    
-    $(this).ajaxSuccess(function(){
-      updateEvents($(this))
-    });
-  
-  });
-  
+  $('div.taxonomy-tree:not(.tt-processed)', context)
+    .addClass('tt-processed')
+		.each(function() {			
+			// treeview behavior added
+			$(this).find('ul').treeview({
+                          url: Drupal.settings.taxonomyTree.path,
+                          toggle: function() {
 
+                            $(this).one('ajaxComplete', function(){    
+                            Drupal.attachBehaviors($(this));
+                            });  
 
+                          }
+                        });
+		})	
+
+  $('ul input.taxonomy-tree-checkbox:not(.tt-processed)', context)
+    .addClass('tt-processed')
+    .click(function(event){
+
+      var $li = $(this).parents('li:eq(0)');
+      var tid = getTid($li.attr('id'));
+
+      $li.attr('id', tid +'-'+ ($(this).is(':checked') ? 1 : 0));  
+      event.stopPropagation();
+
+    })  
+      
 }
 
-updateEvents = function($context){
-  
-   // Prevent clicking the checkbox from expanding the tree
-   
-  $context.find('input').each(function(){
-    
-    $(this).unbind('mousedown');
-    $(this).bind('mousedown', function(){
-    
-      $(this).attr('checked', ($(this).attr('checked') ? 0 : 1)); 
-    
-    });
-    
-    if($(this).is(':checked')){
-      
-      markAsSelected($(this), 1);
-      
-    }
-    
-    $(this).click(function(){
-    
-      self.markAsSelected($(this), ($(this).attr('checked') ? 0 : 1));
-      return false;
-    
-    });
-    
-  })
+getTid = function(id){
 
-}
-
-markAsSelected = function($obj, checked){
-  
-  var $li = $obj.parents('li');
-  var id = $li.attr('id');
-  
   var split_id = [];
   split_id = id.split('-');
+  return split_id[0];
   
-  $li.attr('id',split_id[0]+'-'+checked);
-  
-};
+}
+
 
 
 
