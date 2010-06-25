dd2feae3025b30cdbac0371e76c5030c7ec7498e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-06-25 15:41:36 +0000

    Fully working upload module.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3033 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/dragon/dragon.js
+++ modules/dragon/dragon.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
+Drupal.behaviors.dragon = function(context){
+  Drupal.dragon.init(context);
+};
+
+Drupal.dragon = new Object;
+Drupal.dragon.original_form = false;
+Drupal.dragon.original_drop_event = false;
+
+Drupal.dragon.init = function(context) {
   // Register listeners.
   var dragon = $('.dragon').get(0);
   if(dragon){
     dragons = $.each($('.dragon').get(), function(indexInArray, valueOfElement){
-      valueOfElement.addEventListener('drop', upload, false);
+      valueOfElement.addEventListener('drop', Drupal.dragon.upload, false);
     });
     $('body').get(0).addEventListener('dragenter', function(event){
       $('.dragon').fadeIn(500);
@@ -17,94 +25,103 @@ $(document).ready(function(){
       event.preventDefault(); 
     }, false);
   }
-  
-  // Upload function.
-  function upload(event) {
-    var other_form_bits = $(event.currentTarget).parents('form').serializeArray();
-    var data = event.dataTransfer;
-    /* Show spinner for each dropped file. 
-    
-    for (var i = 0; i < data.files.length; i++) {
-      alert('file');
-      $('#dropzone').append($('<img src="spinner.gif" width="16" height="16" />').css("padding", "48px"));
-    }
-    */
-  
-    var boundary = '------multipartformboundary' + (new Date).getTime();
-    var dashdash = '--';
-    var crlf     = '\r\n';
-    
-    /* Build RFC2388 string. */
-    var builder = '';
-    
-    builder += dashdash;
-    builder += boundary;
-    builder += crlf;
-    
-    var xhr = new XMLHttpRequest();
-    
-    for(var i = 0; i < other_form_bits.length; i++){
-    
-      /* Generate headers. */            
-      builder += 'Content-Disposition: form-data; name="'+other_form_bits[i]['name']+'"';
-      builder += crlf;
-      builder += crlf;
-      
-      // Data
-      builder += other_form_bits[i]['value'];
-      builder += crlf;
-      
-      /* Write boundary. */
-      builder += dashdash;
-      builder += boundary;
-      builder += crlf;
-      
-    }
-    
-    for (var i = 0; i < data.files.length; i++) {
-      var file = data.files[i];
-    
-      /* Generate headers. */            
-      builder += 'Content-Disposition: form-data; name="files[upload]"';
-      if (file.fileName) {
-        builder += '; filename="' + file.fileName + '"';
-      }
-      builder += crlf;
-    
-      builder += 'Content-Type: application/octet-stream';
-      builder += crlf;
-      builder += crlf; 
-      
-      /* Append binary data. */
-      builder += file.getAsBinary();
-      builder += crlf;
-      
-      /* Write boundary. */
-      builder += dashdash;
-      builder += boundary;
-      builder += crlf;
-    }
-    
-    /* Mark end of the request. */
-    builder += dashdash;
-    builder += boundary;
-    builder += dashdash;
-    builder += crlf;
-    
-    xhr.open("POST", Drupal.settings.dragon.callbacks.upload, true);
-    xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + boundary);
-    xhr.sendAsBinary(builder);        
-    
-    xhr.onload = function(event) { 
-    /* If we got an error display it. */
-    if (xhr.responseText) {
-      alert(xhr.responseText);
-    }
-      $(".dragon").load(Drupal.settings.dragon.callbacks.upload);
-    };
-    
-    /* Prevent FireFox opening the dragged file. */
-    event.stopPropagation();
-            
+}
+
+Drupal.dragon.single_file_upload = function(file_num){
+  if(file_num == Drupal.dragon.original_drop_event.dataTransfer.files.length){
+    return;
   }
-});
+  var other_form_bits = $(Drupal.dragon.original_form).serializeArray();
+  /* Show spinner for each dropped file. 
+  
+  for (var i = 0; i < data.files.length; i++) {
+    alert('file');
+    $('#dropzone').append($('<img src="spinner.gif" width="16" height="16" />').css("padding", "48px"));
+  }
+  */
+
+  var boundary = '------multipartformboundary' + (new Date).getTime();
+  var dashdash = '--';
+  var crlf     = '\r\n';
+  
+  /* Build RFC2388 string. */
+  var builder = '';
+  
+  builder += dashdash;
+  builder += boundary;
+  builder += crlf;
+  
+  var xhr = new XMLHttpRequest();
+  
+  for(var j = 0; j < other_form_bits.length; j++){
+  
+    /* Generate headers. */            
+    builder += 'Content-Disposition: form-data; name="'+other_form_bits[j]['name']+'"';
+    builder += crlf;
+    builder += crlf;
+    
+    // Data
+    builder += other_form_bits[j]['value'];
+    builder += crlf;
+    
+    /* Write boundary. */
+    builder += dashdash;
+    builder += boundary;
+    builder += crlf;
+    
+  }
+  
+  var file = Drupal.dragon.original_drop_event.dataTransfer.files[file_num];
+
+  /* Generate headers. */            
+  builder += 'Content-Disposition: form-data; name="files[upload]"';
+  if (file.fileName) {
+    builder += '; filename="' + file.fileName + '"';
+  }
+  builder += crlf;
+
+  builder += 'Content-Type: application/octet-stream';
+  builder += crlf;
+  builder += crlf; 
+  
+  /* Append binary data. */
+  builder += file.getAsBinary();
+  builder += crlf;
+  
+  /* Write boundary. */
+  builder += dashdash;
+  builder += boundary;
+  builder += crlf;
+  
+  /* Mark end of the request. */
+  builder += dashdash;
+  builder += boundary;
+  builder += dashdash;
+  builder += crlf;
+  
+  xhr.open("POST", Drupal.settings.dragon.callbacks.upload, true);
+  xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + boundary);
+  xhr.sendAsBinary(builder);        
+  
+  xhr.onload = function(event) {
+    if (xhr.responseText) {
+      var response = eval('('+ xhr.responseText +')');
+      if(response['status']){
+        $('#attach-wrapper').html(response['data']);
+        $.each(Drupal.behaviors, function() {
+          this();
+        });
+        file_num ++;
+        Drupal.dragon.single_file_upload(file_num);
+      }
+    }
+  };  
+}
+
+Drupal.dragon.upload = function(event) {
+  Drupal.dragon.original_drop_event = event;
+  Drupal.dragon.original_form = $(event.currentTarget).parents('form')
+  Drupal.dragon.single_file_upload(0);     
+  /* Prevent FireFox opening the dragged file. */
+  event.stopPropagation();
+}
