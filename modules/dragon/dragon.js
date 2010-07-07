fc9eb77936aa9e7e7b132fbf3aa06512e5da0c7e
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-07-07 14:19:08 +0000

    Fully working drag'n'drop module.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3059 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/dragon/dragon.js
+++ modules/dragon/dragon.js
@@ -6,8 +6,11 @@ Drupal.dragon.original_form = false;
 Drupal.dragon.original_drop_event = false;
 Drupal.dragon.upload_path = false;
 Drupal.dragon.field_name = false;
+Drupal.dragon.field_name_array = new Array();
+Drupal.dragon.file_num_array = new Array();
 Drupal.dragon.wrapper = false;
 Drupal.dragon.id_parts = false;
+Drupal.dragon.original_drop_box_id = false;
 
 Drupal.dragon.init = function(context) {
   // Register listeners.
@@ -17,13 +20,11 @@ Drupal.dragon.init = function(context) {
       valueOfElement.addEventListener('drop', Drupal.dragon.upload, false);
     });
     $('body').get(0).addEventListener('dragenter', function(event){
-      $('.dragon').animate({height:'100px',width:'99%'}, 1000);
       $('.dragon').css({backgroundColor : '#f5f5b5'});
       $('.dragon').css({border : 'solid 1px #ccc'});
     }, false);
     $('body').get(0).addEventListener('dragexit', function(event){
       if(event.clientX == 0 && event.clientY == 0){
-        $('.dragon').animate({height:'50px',width:'32px'}, 1000);
         $('.dragon').css({backgroundColor : '#fff'});
         $('.dragon').css({border : 'none'});
       }
@@ -34,13 +35,14 @@ Drupal.dragon.init = function(context) {
   }
 }
 
-Drupal.dragon.single_file_upload = function(file_num){
+Drupal.dragon.single_file_upload = function(file_num, offset){
   var boundary = '------multipartformboundary' + (new Date).getTime();
   var dashdash = '--';
   var crlf     = '\r\n';
   
-  if(file_num == Drupal.dragon.original_drop_event.dataTransfer.files.length){
-    return;
+  if(file_num - offset == Drupal.dragon.original_drop_event.dataTransfer.files.length){
+    $('.dragon').css({backgroundColor : '#fff'});
+    $('.dragon').css({border : 'none'});
   }
   temp_field_name = Drupal.dragon.field_name;
   temp_wrapper = Drupal.dragon.wrapper;
@@ -64,40 +66,50 @@ Drupal.dragon.single_file_upload = function(file_num){
   for(var j = 0; j < other_form_bits.length; j++){
     builder += 'Content-Disposition: form-data; name="'+other_form_bits[j]['name']+'"' + crlf + crlf + other_form_bits[j]['value'] + crlf + dashdash + boundary + crlf;
   }
-  var file = Drupal.dragon.original_drop_event.dataTransfer.files[file_num];
-  /* Generate headers. */
-  builder += 'Content-Disposition: form-data; name="'+temp_field_name+'"';
-  if (file.fileName) {
-    builder += '; filename="' + file.fileName + '"';
-  }
-  builder += crlf + 'Content-Type: application/octet-stream' + crlf + crlf + file.getAsBinary() + crlf + dashdash + boundary + crlf + dashdash + boundary + dashdash + crlf;
-  xhr.open("POST", temp_upload_path, true);
-  xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + boundary);
-  xhr.sendAsBinary(builder);
-  xhr.onload = function(event){
-    if (xhr.responseText){
-      var response = eval('('+ xhr.responseText +')');
-      if(response['status']){
-        $('#'+temp_wrapper).html(response['data']);
-        $.each(Drupal.behaviors, function(){
-          this();
-        });
-        file_num ++;
-        Drupal.dragon.single_file_upload(file_num);
-      }
+  var file = Drupal.dragon.original_drop_event.dataTransfer.files[file_num - offset];
+  if(file){
+    /* Generate headers. */
+    builder += 'Content-Disposition: form-data; name="'+temp_field_name+'"';
+    if (file.fileName) {
+      $('#'+Drupal.dragon.original_drop_box_id+' .dragon-message').html(Drupal.t('Uploading file: ')+file.fileName);
+      builder += '; filename="' + file.fileName + '"';
     }
-  };  
+    builder += crlf + 'Content-Type: application/octet-stream' + crlf + crlf + file.getAsBinary() + crlf + dashdash + boundary + crlf + dashdash + boundary + dashdash + crlf;
+    xhr.open("POST", temp_upload_path, true);
+    xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + boundary);
+    xhr.sendAsBinary(builder);
+    xhr.onload = function(event){
+      if (xhr.responseText){
+        var response = eval('('+ xhr.responseText +')');
+        if(response['status']){
+          $('#'+temp_wrapper).html(response['data']);
+          $.each(Drupal.behaviors, function(){
+            this();
+          });
+          file_num ++;
+          Drupal.dragon.file_num_array[Drupal.dragon.original_drop_box_id] = file_num;
+          $('#'+Drupal.dragon.original_drop_box_id+' .dragon-message').html('');
+          Drupal.dragon.single_file_upload(file_num, offset);
+        }
+      }
+    };
+  }
 }
 Drupal.dragon.upload = function(event) {
-  $('.dragon').animate({height:'50px',width:'32px'}, 1000);
-  $('.dragon').css({backgroundColor : '#fff'});
-  $('.dragon').css({border : 'none'});
   Drupal.dragon.original_drop_event = event;
   Drupal.dragon.original_form = $(event.currentTarget).parents('form');
+  Drupal.dragon.original_drop_box_id = event.currentTarget.id;
   Drupal.dragon.id_parts = event.currentTarget.id.split('__dragon__');
   Drupal.dragon.upload_path = Drupal.settings.ahah[Drupal.dragon.id_parts[0]].url;
-  Drupal.dragon.field_name = $('#'+Drupal.settings.ahah[Drupal.dragon.id_parts[0]].wrapper+" input[type=file]").attr('name');
+  if(!Drupal.dragon.field_name_array[Drupal.dragon.id_parts[0]]){
+    Drupal.dragon.field_name_array[Drupal.dragon.id_parts[0]] = $('#'+Drupal.settings.ahah[Drupal.dragon.id_parts[0]].wrapper+" input[type=file]").attr('name');
+  }
+  Drupal.dragon.field_name = Drupal.dragon.field_name_array[Drupal.dragon.id_parts[0]];
   Drupal.dragon.wrapper = Drupal.settings.ahah[Drupal.dragon.id_parts[0]].wrapper;
-  Drupal.dragon.single_file_upload(0);
+  file_num = 0;
+  if(Drupal.dragon.file_num_array[Drupal.dragon.original_drop_box_id]){
+    file_num = Drupal.dragon.file_num_array[Drupal.dragon.original_drop_box_id];
+  }
+  Drupal.dragon.single_file_upload(file_num, file_num);
   event.stopPropagation();
 }
