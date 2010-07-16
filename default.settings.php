d6247cfb4cfc748e251c3db2622912af9bd8ad3c
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-07-16 12:36:40 +0000

    Added memcache. Note, this needs editing for production servers.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3117 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- default.settings.php
+++ default.settings.php
@@ -3,6 +3,21 @@
 $conf = array(
   'file_directory_temp' => '/tmp',  
   'file_directory_path' => 'sites/' . $_SERVER['HTTP_HOST'] . '/files',
+  'cache_inc' => './sites/all/modules/memcache/memcache.inc',
+  'memcache_bins' => array(
+    'cache' => 'default',
+    'cache_filter' => 'filter',
+    'cache_menu' => 'menu',
+    'cache_page' => 'page',
+    'cache_block' => 'block',
+    'cache_content' => 'content',
+    'cache_form' => 'form',
+    'cache_location' => 'location',
+    'cache_mollom' => 'mollom',
+    'cache_term' => 'term',
+    'cache_views' => 'views',
+    'cache_views_data' => 'views_data'
+  )
 );
 
 // Automatic DB, user, password configuration
@@ -23,6 +38,9 @@ if(!isset($user_password[$database])){
   header('Location: http://scratchpads.eu/');
   exit;
 }
+
+// Memcache unique key
+$conf['memcache_key_prefix'] = $database;
 
 $db_url = 'mysqli://' . $user_password[$database]['user'] . ':'. $user_password[$database]['password'] . '@localhost/'. $database;
 $db_prefix = '';
