b9a75935675ca0743f8c6e5e96942e936dc80cdb
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-08-05 09:53:11 +0000

    Reverted change to views module Updated gmap and location module (fixes issue).
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3178 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- contrib/modules/gmap/js/gmap_shapes.js
+++ contrib/modules/gmap/js/gmap_shapes.js
@@ -1,11 +1,14 @@
-/* $Id: gmap_shapes.js,v 1.3 2008/10/20 21:05:08 bdragon Exp $ */
+/* $Id: gmap_shapes.js,v 1.7.2.1 2010/07/03 00:35:46 bdragon Exp $ */
 
 /**
+ * @file
  * GMap Shapes
  * GMap API version / Base case
  */
 
-Drupal.gmap.addHandler('gmap', function(elem) {
+/*global $, Drupal, GEvent, GLatLng, GPolygon, GPolyline */
+
+Drupal.gmap.addHandler('gmap', function (elem) {
   var obj = this;
 /*
   obj.bind('init',function() {
@@ -14,27 +17,27 @@ Drupal.gmap.addHandler('gmap', function(elem) {
     }
   });
 */
-  obj.bind('prepareshape',function(shape) {
-    var i, pa, pp, cargs, style;
+  obj.bind('prepareshape', function (shape) {
+    var pa, cargs, style;
     //var m = new GMarker(new GLatLng(marker.latitude,marker.longitude),marker.opts);
     pa = []; // point array (array of GLatLng-objects)
     var fillstyle = true;
-    if (shape.type == 'circle') {
-      pa = obj.poly.calcPolyPoints(new GLatLng(shape.center[0],shape.center[1]), shape.radius * 1000, shape.numpoints);
+    if (shape.type === 'circle') {
+      pa = obj.poly.calcPolyPoints(new GLatLng(shape.center[0], shape.center[1]), shape.radius * 1000, shape.numpoints);
     }
-    else if (shape.type == 'rpolygon') {
+    else if (shape.type === 'rpolygon') {
       shape.center = new GLatLng(shape.center[0], shape.center[1]);
       shape.point2 = new GLatLng(shape.point2[0], shape.point2[1]);
       var radius = shape.center.distanceFrom(shape.point2);
       pa = obj.poly.calcPolyPoints(shape.center, radius, shape.numpoints);
     }
-    else if (shape.type == 'polygon') {
-      $.each(shape.points, function(i, n) {
+    else if (shape.type === 'polygon') {
+      $.each(shape.points, function (i, n) {
         pa.push(new GLatLng(n[0], n[1]));
       });
     }
-    else if (shape.type == 'line') {
-      $.each(shape.points, function(i, n) {
+    else if (shape.type === 'line') {
+      $.each(shape.points, function (i, n) {
         pa.push(new GLatLng(n[0], n[1]));
       });
       fillstyle = false;
@@ -43,13 +46,13 @@ Drupal.gmap.addHandler('gmap', function(elem) {
 
     // Style normalization
     if (fillstyle) {
-      style = obj.vars.styles['poly_default'].slice();
+      style = obj.vars.styles.poly_default.slice();
     }
     else {
-      style = obj.vars.styles['line_default'].slice();
+      style = obj.vars.styles.line_default.slice();
     }
     if (shape.style) {
-      if (typeof shape.style == 'string') {
+      if (typeof shape.style === 'string') {
         if (obj.vars.styles[shape.style]) {
           style = obj.vars.styles[shape.style].slice();
         }
@@ -65,16 +68,36 @@ Drupal.gmap.addHandler('gmap', function(elem) {
       style[3] = '#' + style[3];
       style[4] = style[4] / 100;
     }
+    
+    if (shape.type == 'encoded_line') {
+      shape.color = style[0];
+      shape.weight = style[1];
+      shape.opacity = style[2];
+    }
+    else if (shape.type == 'encoded_polygon') {
+      $.each(shape.polylines, function(i, polyline) {
+        polyline.color = style[0];
+        polyline.weight = style[1];
+        polyline.opacity = style[2];
+      });
+      shape.fill = true;
+      shape.color = style[3];
+      shape.opacity = style[4];
+      shape.outline = true;
+    }
 
-    $.each(style, function(i,n){
+    $.each(style, function (i, n) {
       cargs.push(n);
     });
-    var Pg = function(args) {
-      GPolygon.apply(this,args);
+    if (shape.opts) {
+      cargs.push(shape.opts);
+    }
+    var Pg = function (args) {
+      GPolygon.apply(this, args);
     };
     Pg.prototype = new GPolygon();
-    var Pl = function(args) {
-      GPolyline.apply(this,args);
+    var Pl = function (args) {
+      GPolyline.apply(this, args);
     };
     Pl.prototype = new GPolyline();
     switch (shape.type) {
@@ -86,28 +109,36 @@ Drupal.gmap.addHandler('gmap', function(elem) {
       case 'line':
         shape.shape = new Pl(cargs);
         break;
+      case 'encoded_line':
+        shape.shape = GPolyline.fromEncoded(shape);        
+        break;
+      case 'encoded_polygon':
+        shape.shape = GPolygon.fromEncoded(shape);
+        break;
     }
   });
 
-  obj.bind('addshape', function(shape) {
+  obj.bind('addshape', function (shape) {
     if (!obj.vars.shapes) {
       obj.vars.shapes = [];
     }
     obj.vars.shapes.push(shape);
     obj.map.addOverlay(shape.shape);
-    //if (obj.vars.behavior.autozoom) {
-    //  obj.bounds.extend(marker.marker.getPoint());
-    //  obj.map.setCenter(obj.bounds.getCenter(),obj.map.getBoundsZoomLevel(obj.bounds));
-    //}
+
+    if (obj.vars.behavior.clickableshapes) {
+      GEvent.addListener(shape.shape, 'click', function () {
+        obj.change('clickshape', -1, shape);
+      });
+    }
   });
 
-  obj.bind('delshape',function(shape) {
+  obj.bind('delshape', function (shape) {
     obj.map.removeOverlay(shape.shape);
   });
 
-  obj.bind('clearshapes',function() {
+  obj.bind('clearshapes', function () {
     if (obj.vars.shapes) {
-      $.each(obj.vars.shapes, function(i,n) {
+      $.each(obj.vars.shapes, function (i, n) {
         obj.change('delshape', -1, n);
       });
     }
