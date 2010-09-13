7443d9e29b95cce57df51e36e67fd4f317751615
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-09-13 16:50:46 +0000

    Improved the way the tab is created.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3280 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_block/remote_issue_block.js
+++ modules/remote_issue_block/remote_issue_block.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.remote_issues_block = function(context){
   var remote_issues_block_hidden = true;
-  $('#block-remote_issue_block-0', context).click(function(){
+  $('#block-remote_issue_block-0 #open-close', context).click(function(){
     if(remote_issues_block_hidden){
       $('#block-remote_issue_block-0').animate({right:0}, 1000);
       remote_issues_block_hidden = false;
