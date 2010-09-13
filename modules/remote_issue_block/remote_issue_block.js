2f8704ee8d1c9466328dbcc16e9f4bb6d35dd172
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-09-13 15:53:34 +0000

    Add uwho stuff.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3277 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_block/remote_issue_block.js
+++ modules/remote_issue_block/remote_issue_block.js
@@ -1,12 +1,11 @@
 Drupal.behaviors.remote_issues_block = function(context){
   var remote_issues_block_hidden = true;
   $('#block-remote_issue_block-0', context).click(function(){
-    console.log(this);
     if(remote_issues_block_hidden){
       $('#block-remote_issue_block-0').animate({right:0}, 1000);
       remote_issues_block_hidden = false;
     } else {
-      $('#block-remote_issue_block-0').animate({right:-390}, 1000);
+      $('#block-remote_issue_block-0').animate({right:-375}, 1000);
       remote_issues_block_hidden = true;      
     }
   });
