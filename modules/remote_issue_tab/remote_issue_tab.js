5bc15c10c37f16d6d6daf34cd1cf762e10f3663a
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-09-14 13:41:51 +0000

    Tweaks that I don't want to lose.  Not finished moving the block to the footer.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3297 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_tab/remote_issue_tab.js
+++ modules/remote_issue_tab/remote_issue_tab.js
@@ -1,12 +1,12 @@
 Drupal.behaviors.remote_issues_block = function(context){
   var remote_issues_block_hidden = true;
-  $('#block-remote_issue_tab-0 #open-close', context).click(function(){
+  $('#remote-issue-tab #open-close', context).click(function(){
     if(remote_issues_block_hidden){
-      $('#block-remote_issue_tab-0').css('z-index', '10');
-      $('#block-remote_issue_tab-0').animate({right:0}, 1000);
+      $('#remote-issue-tab').css('z-index', '10');
+      $('#remote-issue-tab').animate({right:0}, 1000);
       remote_issues_block_hidden = false;
     } else {
-      $('#block-remote_issue_tab-0').animate({right:-375}, 1000);
+      $('#remote-issue-tab').animate({right:-375}, 1000);
       remote_issues_block_hidden = true;      
     }
   });
