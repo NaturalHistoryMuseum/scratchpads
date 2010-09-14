760ede9b591b6c2151fbde3540dda47888d2da66
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-09-14 12:24:18 +0000

    Renamed, as due to z-index issues, it's easier to display in the footer of the page. Also, it doesn't make sense giving it the flexibility of a block, when it isn't actually flexible.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3290 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_tab/remote_issue_block.js
+++ modules/remote_issue_tab/remote_issue_block.js
@@ -2,6 +2,7 @@ Drupal.behaviors.remote_issues_block = function(context){
   var remote_issues_block_hidden = true;
   $('#block-remote_issue_block-0 #open-close', context).click(function(){
     if(remote_issues_block_hidden){
+      $('#block-remote_issue_block-0').css('z-index', '10');
       $('#block-remote_issue_block-0').animate({right:0}, 1000);
       remote_issues_block_hidden = false;
     } else {
