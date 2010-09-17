59cf0672ed712c6600ce12861f12e7fe8d482453
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-09-17 12:48:13 +0000

    Set the z-index in the CSS file.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3338 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_tab/remote_issue_tab.js
+++ modules/remote_issue_tab/remote_issue_tab.js
@@ -2,7 +2,6 @@ Drupal.behaviors.remote_issues_block = function(context){
   var remote_issues_block_hidden = true;
   $('#remote-issue-tab #open-close', context).click(function(){
     if(remote_issues_block_hidden){
-      $('#remote-issue-tab').css('z-index', '10');
       $('#remote-issue-tab').animate({right:0}, 1000);
       remote_issues_block_hidden = false;
     } else {
