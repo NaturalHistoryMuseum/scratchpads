dd57f8664d67f35ba99e146ac0056a3913cb1f7d
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-10-29 12:03:11 +0000

    Simple module that is tuned specifically for the admin site.  This must be edited if the admin site is rebuilt (hence why this module is a little pants).
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3545 ccd0800f-5989-4abc-b5cb-522e550fd16a

---
+++ modules/hostmaster_tweak/hostmaster_tweak.js
@@ -0,0 +1,28 @@
+Drupal.hostmaster_tweak = new Object;
+
+Drupal.hostmaster_tweak.do_search = function(copy){
+  if(copy){
+    var domain = $('#edit-title-copy').val();    
+  }else{
+    var domain = $('#edit-title').val();
+  }
+  if(domain.length>10){
+    if(domain.substring(domain.length-10) == 'taxon.name'){
+      $('#edit-profile-696').attr('checked', 'checked');
+      $('#edit-platform-767').attr('checked', 'checked');
+      $('#edit-db-server-721').attr('checked', 'checked');
+    }    
+  }
+  return false;
+}
+
+Drupal.behaviors.hostmaster_tweak = function(context){
+  $('#edit-title', context).keyup(function(){
+    Drupal.hostmaster_tweak.do_search(false);
+    return false;
+  });
+  $('#edit-title-copy', context).keyup(function(){
+    Drupal.hostmaster_tweak.do_search(true);
+    return false;
+  });
+};
