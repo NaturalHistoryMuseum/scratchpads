36c8c886cea36cd9043eb7f8eea97ab54f7d1deb
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-11-01 17:58:35 +0000

    Huh, why wasn't this added first time round.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3551 ccd0800f-5989-4abc-b5cb-522e550fd16a

---
+++ modules/vbranteu/vbranteu.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.partnersmapinstitution').bt({positions: 'left',fill: 'rgba(0, 0, 0, .7)',cssStyles: {color: 'white', 'font-size': '14px', width: 'auto'},closeWhenOthersOpen: true,spikeLength: 10,strokeWidth: 0});
+  $('.partnersmapinstitution').click(function(){
+    $('#partnerswps li a').css('color','');
+    $('#partnerswps li').css('background-color','transparent');
+    $('.partnersmapinstitution').css('border', 'none');
+    $(this).css('border', 'solid 2px yellow');
+    var fadein_id = '#'+$(this).attr('id')+'_info';
+    if($('#partnersinfo div:visible').length){
+      $('#partnersinfo div:visible').fadeOut('500', function(){
+        $(fadein_id).fadeIn('500');
+      });
+    } else {
+      $(fadein_id).fadeIn('500');
+    }
+  });
+  $('#partnerswps li').click(function(){
+    $('#partnerswps li a').css('color','');
+    $('#partnerswps li').css('background-color','transparent');
+    $(this).css('background-color','black');
+    $(this).children('a').css('color','white');
+    $('.partnersmapinstitution').css('border', 'none');
+    $('.partnersmapinstitution.'+$(this).attr('id')).css('border', 'solid 2px yellow');
+    var fadein_id = '#'+$(this).attr('id')+'_info';
+    if($('#partnersinfo div:visible').length){
+      $('#partnersinfo div:visible').fadeOut('500', function(){
+        $(fadein_id).fadeIn('500');
+      });
+    } else {
+      $(fadein_id).fadeIn('500');
+    }
+  });
+});
