20863c385d92acf62a4c01137901e4179dbdc5eb
Simon Rycroft <s.rycroft@nhm.ac.uk>
2010-12-09 17:30:56 +0000

    Small bug - left a console.log in there.
    git-svn-id: http://svn.scratchpads.eu/svn/scratchpads/trunk@3711 ccd0800f-5989-4abc-b5cb-522e550fd16a

--- modules/remote_issue_tab/remote_issue_tab.js
+++ modules/remote_issue_tab/remote_issue_tab.js
@@ -12,7 +12,6 @@ Drupal.behaviors.remote_issues_block = function(context){
   $('.items li').bind('mouseenter', function(){
     $(this).children().children('p').slideDown(500);
     $(this).siblings().each(function(){
-      console.log($(this));
       $(this).children().children('p').slideUp(500);
     });
   });
