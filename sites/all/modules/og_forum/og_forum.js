5f4f5c0acb1eefac4b583aed75ca82e1ed45290c
Simon Rycroft <s.rycroft@nhm.ac.uk>
2012-06-20 09:27:15 +0100

    Possible fix for the issue with forums on Scratchpads 1.0.

--- sites/all/modules/og_forum/og_forum.js
+++ sites/all/modules/og_forum/og_forum.js
@@ -1,4 +1,3 @@
-// $Id: og_forum.js,v 1.1.2.1 2008/07/22 14:33:56 paulbooker Exp $
 
 Drupal.ogAttach = function() {
 /*  Disable the public checkbox if no groups are selected in in Audience, unless audience is required and set in hidden field*/
